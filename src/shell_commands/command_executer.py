e208f554706b810d39d55d1c0c09cc988a684c2f
FluxIX <chris.d.herrick@gmail.com>
2015-05-15 06:40:20 -0500

    Updated the .gitignore file. Reformatted the command_executer.py file.

--- src/shell_commands/command_executer.py
+++ src/shell_commands/command_executer.py
@@ -1,10 +1,10 @@
-class CommandExecuter(object):
-    def __init__(self):
+class CommandExecuter( object ):
+    def __init__( self ):
         pass
-    
-    def execute_commands(self,commands):
+
+    def execute_commands( self, commands ):
         import subprocess
-        
+
         for command in commands:
-#             subprocess.check_call(command.terms)
-            print( "Executing command: " + str( command ) )
+            subprocess.check_call( command.terms )
+#            print( "Executing command: " + str( command ) )
