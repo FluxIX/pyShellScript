235054953b4e9bc0bbbdc132a6264ab1efbdac95
FluxIX <chris.d.herrick@gmail.com>
2015-05-24 02:40:06 -0500

    Altered some optional parameters on the Linux commands. Also made use of the _get_value helper function.

--- src/pyshell/linux/commands.py
+++ src/pyshell/linux/commands.py
@@ -12,10 +12,10 @@ class SudoCommand( LinuxCommand ):
     def get_command( self ):
         return self._command
 
-    def set_command( self, value ):
+    def _set_command( self, value ):
         self._command = value
 
-    command = property( get_command, set_command, None, None )
+    command = property( get_command, _set_command, None, None )
 
 class ServiceCommand( LinuxCommand ):
     class Operations( object ):
@@ -48,42 +48,38 @@ class MakeCommand( LinuxCommand ):
     def __init__( self, *options, **kwargs ):
         parameters = []
 
-        key = MakeCommand.OptionalParameters.Target
-        if key in kwargs:
-            value = kwargs[ key ]
-            if value is not None:
-                parameters.append( value )
+        target_value = self._get_value( kwargs, MakeCommand.OptionalParameters.Target, None )
+        if target_value is not None:
+            parameters.append( target_value )
 
         LinuxCommand.__init__( self, "make", parameters, *options, **kwargs )
 
     def get_directory( self ):
         return self._directory
 
-    def set_directory( self, value ):
+    def _set_directory( self, value ):
         self._directory = value
 
-    directory = property( get_directory, set_directory, None, None )
+    directory = property( get_directory, _set_directory, None, None )
 
 class ListDirectoryCommand( LinuxCommand ):
     class OptionalParameters( object ):
-        Directory = "dir"
+        Directory = "directory"
 
     def __init__( self, *options, **kwargs ):
         parameters = []
 
-        key = ListDirectoryCommand.OptionalParameters.Directory
-        if key in kwargs:
-            value = kwargs[ key ]
-            if value is not None:
-                parameters.append( value )
+        directory = self._get_value( kwargs, ListDirectoryCommand.OptionalParameters.Directory )
+        if directory is not None:
+            parameters.append( directory )
 
         LinuxCommand.__init__( self, "ls", parameters, *options, **kwargs )
 
     def get_directory( self ):
         return self._directory
 
-    def set_directory( self, value ):
+    def _set_directory( self, value ):
         self._directory = value
 
-    directory = property( get_directory, set_directory, None, None )
+    directory = property( get_directory, _set_directory, None, None )
 
